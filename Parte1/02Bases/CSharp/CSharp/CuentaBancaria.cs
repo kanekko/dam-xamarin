1b22fecdb86a160f4b3c5f1523b7e6230ec76dc2
Canek García <kaan.ek@ciencias.unam.mx>
2019-05-24 14:11:27 -0500

    moved bases project

---
+++ Parte1/02Bases/CSharp/CSharp/CuentaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace CSharp
+{
+    interface ICuentaBancaria
+    {
+        double Balnce { get; set; }
+        double AgregarABalance(double balanceAAgregar);
+    }
+
+
+    public class CuentaBancaria : ICuentaBancaria
+    {
+        private double balance;
+        public double Balnce
+        {
+            get { return balance; }
+            set
+            {
+                if (value > 0)
+                    balance = value;
+                else
+                    balance = 0;
+            }
+        }
+
+        public double AgregarABalance(double balanceAAgregar)
+        {
+            Balnce = Balnce + balanceAAgregar;
+            return Balnce;
+        }
+    }
+
+
+
+
+}
---
+++ Parte1/02Bases/CSharp/CSharp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            CuentaBancaria cuentaUno = new CuentaBancaria();
+            cuentaUno.Balnce = 1000;
+            cuentaUno.AgregarABalance(3000);
+            Console.WriteLine($"Resultado: {cuentaUno.Balnce}");
+            Console.ReadLine();
+        }
+
+    }
+
+
+    class Matematicas
+    {
+        public static int Suma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public static int Suma(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+
+        public static int Resta(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        public static int Multiplicacion(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        public static int Division(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+
+    }
+
+    class Geometria : Matematicas
+    {
+        public static int AreaCuadrada(int lado)
+        {
+            // return lado * lado;
+            return Multiplicacion(lado, lado);
+        }
+
+        public override int Suma(int n1, int n2)
+        {
+            return n2 ** n1;
+        }
+    }
+
+}
