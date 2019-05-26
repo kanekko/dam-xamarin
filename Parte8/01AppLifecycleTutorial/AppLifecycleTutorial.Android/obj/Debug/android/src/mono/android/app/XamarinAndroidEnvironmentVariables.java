f5c6619aa676469f073ba2519f3266a5158dca02
Canek García <kaan.ek@ciencias.unam.mx>
2019-05-26 10:42:31 -0500

    Added lifecycle project

---
+++ Parte8/01AppLifecycleTutorial/AppLifecycleTutorial.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "a9775a43-f2f4-4ab1-a3c8-a193a1e6d72e",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep",
+
+	};
+}
