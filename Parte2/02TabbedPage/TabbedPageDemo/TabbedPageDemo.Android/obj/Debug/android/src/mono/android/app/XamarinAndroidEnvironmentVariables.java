704bc5878db82f6ad1987cb890df537c1f164bc7
Canek García <kaan.ek@ciencias.unam.mx>
2019-05-26 10:36:51 -0500

    Added MVVM project

---
+++ Parte2/02TabbedPage/TabbedPageDemo/TabbedPageDemo.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "aa518b6f-bcb8-4a86-b8f1-03d0264cb3d5",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep",
+
+	};
+}
